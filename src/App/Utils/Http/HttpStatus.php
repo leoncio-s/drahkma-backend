94826a8ed6cb447fd3296d40564e4bf18d97bd56
grrypsing <leonciosouza01@hotmail.com>
2026-04-05 22:47:15 -0300

    feat: add case HTTP_NOT_MODIFIED = 304

--- src/App/Utils/Http/HttpStatus.php
+++ src/App/Utils/Http/HttpStatus.php
@@ -7,6 +7,7 @@ enum HttpStatus: int
     case HTTP_OK = 200;
     case HTTP_CREATED = 201;
     case HTTP_NO_CONTENT = 204;
+    case HTTP_NOT_MODIFIED = 304;
     case HTTP_BAD_REQUEST = 400;
     case HTTP_UNAUTHORIZED = 401;
     case HTTP_FORBIDDEN = 403;
