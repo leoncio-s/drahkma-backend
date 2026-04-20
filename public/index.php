88586138302a5eb57e4e9d542028abab93ab6851
grrypsing <leonciosouza01@hotmail.com>
2026-04-20 00:46:11 -0300

    feat: set session parameters

--- public/index.php
+++ public/index.php
@@ -9,3 +9,18 @@ require __DIR__ . '/../exception_handler.php';
 require __DIR__ . '/../config.php';
 
 require __DIR__ . '/../routes.php';
+
+
+session_start([
+'cookie_httponly' => true,
+'cookie_secure' => true,
+'cookie_samesite' => 'Strict',
+'use_strict_mode' => true,
+'cookie_lifetime' => 86400,
+'read_and_close'  => true,
+ 'name'=> 'DRAHKMASESS'
+]);
+
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
