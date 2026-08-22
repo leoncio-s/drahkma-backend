1072eb13654d358dec33d66f2cd4b45bf9d8751d
grrypsing <leonciosouza01@hotmail.com>
2026-08-21 23:31:06 -0300

    refactore: remove unused methods and add types

--- src/Utils/Base64Utils.php
+++ src/Utils/Base64Utils.php
@@ -5,25 +5,8 @@ namespace App\Utils;
 class Base64Utils
 {
 
-
-    public static function base64_url_encode(string $data): string
-    {
-        $base = base64_encode($data);
-
-        return urlencode($base);
-    }
-
-
-    public static function base64_url_decode(string $data): string
-    {
-        $base = base64_decode($data);
-
-        return urldecode($base);
-    }
-
-
     /// https://base64.guru/developers/php/examples/base64url#:~:text=PHP%20doesn%E2%80%99t%20support%20the%20Base64URL%20standard%2C%20but%20you,instead%20of%20%E2%80%9C%2B%E2%80%9D%20and%20%E2%80%9C_%E2%80%9D%20instead%20of%20%E2%80%9C%2F%E2%80%9D.
-    public static function base64url_encode($data)
+    public static function base64url_encode(string $data) : string
     {
         // First of all you should encode $data to Base64 string
         $b64 = base64_encode($data);
@@ -46,7 +29,7 @@ class Base64Utils
      * @param boolean $strict
      * @return boolean|string
      */
-    public static function base64url_decode($data, $strict = false)
+    public static function base64url_decode(string $data, bool $strict = false) : string
     {
         // Convert Base64URL to Base64 by replacing “-” with “+” and “_” with “/”
         $b64 = strtr($data, '-_', '+/');
